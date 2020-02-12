ce0ba414664127476a96dbc2485d41d769dd0317
Nathan Woulfe <nathan@nathanw.com.au>
2020-02-12 15:08:41 +1000

    kill interval when controller scope destroyed

--- src/Preserver/backoffice/editor.controller.js
+++ src/Preserver/backoffice/editor.controller.js
@@ -1,6 +1,6 @@
 ﻿(() => {
 
-    function preserver($interval, $rootScope, $element, editorState, notificationsService) {
+    function preserver($scope, $interval, $rootScope, $element, editorState, notificationsService) {
 
         const dataKey = `preserver_data_${editorState.current.id}`;
 
@@ -17,7 +17,7 @@
         /**
          * map local values back onto model
          */
-        const map = model => {
+        const mapToEditorModel = model => {
             for (let m of model) {
                 let editorVariant = editorState.current.variants.find(x => x.language.culture === m.variant);
                 if (editorVariant) {
@@ -57,7 +57,7 @@
                             alias: t.alias,
                             properties: t.properties.filter(x => x.editor != 'preserver').map(p => {
                                 return {
-                                    alias: p.alias,
+                                    alias: p.alias, 
                                     value: p.value
                                 }
                             })
@@ -71,7 +71,7 @@
             return JSON.stringify(model);
         };
 
-
+ 
         /**
          * hide the property editor
          */
@@ -97,7 +97,7 @@
          */
         $rootScope.$on('preserver.update', (e, data) => {
             if (data.id === editorState.current.id) {
-                map(JSON.parse(fromStore));
+                mapToEditorModel(JSON.parse(fromStore));
             }
         });
 
@@ -107,12 +107,24 @@
          */
         $rootScope.$on('content.saved', (e, data) => localStorage.removeItem(dataKey));
 
+        
+        /**
+         * make sure the interval is killed along with the controller - otherwise continues to fire when changing sections.
+         */
+        $scope.$on('$destroy', () => {
+            if (angular.isDefined(interval)) {
+                $interval.cancel(interval);
+                interval = undefined;
+            }
+        });
+        
 
         /**
          * smash it into local storage every 10 seconds, if the node is dirty
          */
+        let interval;
         if (editorState.current.variants) {
-            $interval(() => {
+            interval = $interval(() => {
                 if (editorState.current.variants.some(x => x.isDirty)) {
                     localStorage.setItem(dataKey, getBasicModel(editorState.current));
                 }
@@ -122,5 +134,5 @@
 
     angular.module('preserver')
         .controller('preserver.editor.controller',
-            ['$interval', '$rootScope', '$element', 'editorState', 'notificationsService', preserver]);
+            ['$scope', '$interval', '$rootScope', '$element', 'editorState', 'notificationsService', preserver]);
 })();
