f38fb78813dfa5af62b9ff9fd0c86c2b1c47a412
Nathan Woulfe <nathan@nathanw.com.au>
2021-05-06 09:28:48 +1000

    build on vs 19

--- BuildPackage/Build.bat
+++ BuildPackage/Build.bat
@@ -5,6 +5,6 @@ ECHO APPVEYOR_BUILD_VERSION : %APPVEYOR_BUILD_VERSION%
 ECHO PACKAGE_SUFFIX : %UMBRACO_PACKAGE_PRERELEASE_SUFFIX%
 cd ..\
 Call npm install
-Call gulp --production --buildversion=%APPVEYOR_BUILD_VERSION% --buildbranch=%APPVEYOR_REPO_BRANCH% --packagesuffix=%UMBRACO_PACKAGE_PRERELEASE_SUFFIX%
+Call gulp build --prod --buildversion=%APPVEYOR_BUILD_VERSION% --buildbranch=%APPVEYOR_REPO_BRANCH% --packagesuffix=%UMBRACO_PACKAGE_PRERELEASE_SUFFIX%
 cd BuildPackage
-Call "%programfiles(x86)%\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe" package.proj
+Call "%programfiles(x86)%\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MSBuild.exe" package.proj
